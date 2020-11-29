df3e39d492cd0c52abe2f6a3516d6f130d28809e
SuperGibbon <super_gibbon@hotmail.com>
2020-11-29 12:28:48 +0000

    add js to hide why play and make other elements visible on button click

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,12 @@
+/* Nav bar buttons */
+
 $("#whyPlayBtn").click(function() {
     $(".aboutHidden").addClass("d-none")
+    $(".card-container").addClass("d-none")
     $(".whyPlay-container").removeClass("d-none")
+})
+
+$("#homeBtn").click(function() {
+    $(".whyPlay-container").addClass("d-none")
+    $(".aboutHidden").removeClass("d-none")  
 })
